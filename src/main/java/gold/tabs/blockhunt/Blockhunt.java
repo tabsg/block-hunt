ccdf7e1e2e7a1746ca04202cece4408238f60ed6
tabsg <tabitha.goldman@btinternet.com>
2021-04-10 23:16:47 +0100

    add block assignment

--- src/main/java/gold/tabs/blockhunt/Blockhunt.java
+++ src/main/java/gold/tabs/blockhunt/Blockhunt.java
@@ -2,8 +2,11 @@ package gold.tabs.blockhunt;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -15,8 +18,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Blockhunt extends JavaPlugin {
 
   // private static final List<Material> blockChoices = Arrays.asList(Material.values());
-  private static final Set<Material> blockChoices =
-      new HashSet<>(Arrays.asList(Material.GRASS_BLOCK, Material.STONE, Material.DIRT));
+  private static final List<Material> blockChoices =
+      new ArrayList<>(Arrays.asList(Material.GRASS_BLOCK, Material.STONE, Material.DIRT));
 
   @Override
   public void onEnable() {
@@ -47,5 +50,16 @@ public final class Blockhunt extends JavaPlugin {
     // allocate points
     List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
     System.out.println(players);
+    Map<Player, Material> playerBlockMap = generateBlocks(players);
+  }
+
+  private Map<Player, Material> generateBlocks(List<Player> players) {
+    Random random = new Random();
+    Map<Player, Material> playerBlockMap = new HashMap<>();
+    for (Player player : players) {
+      int index = random.nextInt(blockChoices.size());
+      playerBlockMap.put(player, blockChoices.get(index));
+    }
+    return playerBlockMap;
   }
 }
