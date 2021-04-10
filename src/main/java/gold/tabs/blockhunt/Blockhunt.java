22afe97240b058e343115b9b02606158892368f5
tabsg <tabitha.goldman@btinternet.com>
2021-04-10 23:10:00 +0100

    add block choices, list of players and check for command

--- src/main/java/gold/tabs/blockhunt/Blockhunt.java
+++ src/main/java/gold/tabs/blockhunt/Blockhunt.java
@@ -1,17 +1,51 @@
 package gold.tabs.blockhunt;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Blockhunt extends JavaPlugin {
 
+  // private static final List<Material> blockChoices = Arrays.asList(Material.values());
+  private static final Set<Material> blockChoices =
+      new HashSet<>(Arrays.asList(Material.GRASS_BLOCK, Material.STONE, Material.DIRT));
+
   @Override
   public void onEnable() {
-    // Plugin startup logic
-
+    System.out.println(blockChoices);
   }
 
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (command.getName().equalsIgnoreCase("blockhunt")) {
+      play();
+      return true;
+    }
+    return false;
+  }
+
+  private void play() {
+    // get players
+    // generate blocks
+    // tell players their blocks
+    // start a timer
+    // countdown last 5 seconds
+    // at end of timer, check blocks
+    // allocate points
+    List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+    System.out.println(players);
   }
 }
