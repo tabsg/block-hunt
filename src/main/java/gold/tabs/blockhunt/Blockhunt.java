c6bfd5103576e014580ca334a928f6b738e8ac75
tabsg <tabitha.goldman@btinternet.com>
2021-04-10 22:48:33 +0100

    initial commit

---
+++ src/main/java/gold/tabs/blockhunt/Blockhunt.java
@@ -0,0 +1,17 @@
+package gold.tabs.blockhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Blockhunt extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
